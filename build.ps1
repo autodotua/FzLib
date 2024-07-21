7842887eaba1f34d197d80280777030fe3a55bbc
autodotua <autodotua@outlook.com>
2024-07-21 20:46:53 +0800

    增加Nuget打包相关脚本

--- build.ps1
+++ build.ps1
@@ -1,2 +1,5 @@
 dotnet build FzLib.Avalonia/FzLib.Avalonia.csproj --configuration Release
 dotnet build FzLib.Windows --configuration Release
+dotnet pack .\FzLib\FzLib.csproj
+dotnet pack .\FzLib.Windows\FzLib.Windows.csproj
+dotnet pack .\FzLib.Avalonia\FzLib.Avalonia.csproj
