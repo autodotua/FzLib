08743bd058dbf96c53ae0b24fc5278646f939c4d
f-shake <autodotua@outlook.com>
2025-07-20 18:07:52 +0800

    3.0.0-beta.1

--- FzLib.Avalonia/Converters/Basics/Equal2BoolConverter.cs
+++ FzLib.Avalonia/Converters/Basics/Equal2BoolConverter.cs
@@ -8,9 +8,13 @@ namespace FzLib.Avalonia.Converters;
 
 public class Equal2BoolConverter : Value2BoolConverter<object>
 {
+    public StringComparison StringComparison { get; set; } = StringComparison.OrdinalIgnoreCase;
+
     protected override bool ConvertImpl(object value, Type targetType, object parameter, CultureInfo culture)
     {
         Debug.Assert(value != null);
-        return value.Equals(parameter);
+        return value.Equals(parameter)
+               || value is string strValue && strValue.Equals(parameter?.ToString(), StringComparison)
+               || parameter is string strParam && strParam.Equals(value?.ToString(), StringComparison);
     }
 }
