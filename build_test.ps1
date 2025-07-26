0f86138b506bac3739818404a763508ae7f58a79
f-shake <autodotua@outlook.com>
2025-07-26 11:16:12 +0800

    更新版本号

--- build_test.ps1
+++ build_test.ps1
@@ -11,13 +11,13 @@
     Clear-Host
 
     Write-Output "正在发布win-x64"
-    dotnet publish FzLib.Avalonia/Test/Desktop -r win-x64 -c Release -o Publish/Test/Windows --self-contained true /p:PublishSingleFile=$s 
+    dotnet publish FzLib.Avalonia/FzLib.Avalonia.Test.csproj -r win-x64 -c Release -o Publish/Test/Windows --self-contained true /p:PublishSingleFile=$s 
    
     Write-Output "正在发布linux-x64"
-    dotnet publish FzLib.Avalonia/Test/Desktop -r linux-x64 -c Release -o Publish/Test/Linux --self-contained true /p:PublishSingleFile=$s 
+ #   dotnet publish FzLib.Avalonia/FzLib.Avalonia.Test.csproj -r linux-x64 -c Release -o Publish/Test/Linux --self-contained true /p:PublishSingleFile=$s 
    
     Write-Output "正在发布macos-x64"
-    dotnet publish FzLib.Avalonia/Test/Desktop -r osx-x64 -c Release -o Publish/Test/MacOS --self-contained true /p:PublishSingleFile=$s 
+#    dotnet publish FzLib.Avalonia/FzLib.Avalonia.Test.csproj -r osx-x64 -c Release -o Publish/Test/MacOS --self-contained true /p:PublishSingleFile=$s 
     
     Write-Output "操作完成"
 
