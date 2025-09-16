440ee7bd1a07ac232cb29c93c3766aaac25882d3
fshake <f-shake@outlook.com>
2025-09-16 12:58:29 +0800

    修复了一些JsonSerializableExtensions的BUG

--- FzLib/Text/JsonSerializableExtensions.cs
+++ FzLib/Text/JsonSerializableExtensions.cs
@@ -9,6 +9,8 @@ public static class JsonSerializableExtensions
 {
     public static T FromJson<T>(this IJsonSerializableFactory factory, string json)
     {
+        ArgumentNullException.ThrowIfNull(factory);
+        ArgumentException.ThrowIfNullOrWhiteSpace(json);
         return (T)JsonSerializer.Deserialize(json, typeof(T), factory.Context);
     }
 
@@ -42,19 +44,64 @@ public static class JsonSerializableExtensions
 
     public static T LoadJsonFile<T>(this IJsonFileSerializableFactory factory)
     {
+        ArgumentNullException.ThrowIfNull(factory);
+        ArgumentException.ThrowIfNullOrWhiteSpace(factory.FileName);
+        if (!File.Exists(factory.FileName))
+        {
+            throw new FileNotFoundException($"文件{factory.FileName}不存在");
+        }
         string json = File.ReadAllText(factory.FileName);
+        ArgumentException.ThrowIfNullOrWhiteSpace(json);
+        return (T)JsonSerializer.Deserialize(json, typeof(T), factory.Context);
+    }
+
+    public static T LoadJsonFile<T>(this IJsonFileSerializableFactory factory, Func<T> defaultValue)
+    {
+        ArgumentNullException.ThrowIfNull(factory);
+        ArgumentException.ThrowIfNullOrWhiteSpace(factory.FileName);
+        if (!File.Exists(factory.FileName))
+        {
+            return defaultValue();
+        }
+
+        string json = File.ReadAllText(factory.FileName);
+        ArgumentException.ThrowIfNullOrWhiteSpace(json);
         return (T)JsonSerializer.Deserialize(json, typeof(T), factory.Context);
     }
 
     public static async Task<T> LoadJsonFileAsync<T>(this IJsonFileSerializableFactory factory,
+            CancellationToken ct = default)
+    {
+        ArgumentNullException.ThrowIfNull(factory);
+        ArgumentException.ThrowIfNullOrWhiteSpace(factory.FileName);
+        if (!File.Exists(factory.FileName))
+        {
+            throw new FileNotFoundException($"文件{factory.FileName}不存在");
+        }
+        string json = await File.ReadAllTextAsync(factory.FileName, ct);
+        ArgumentException.ThrowIfNullOrWhiteSpace(json);
+        return (T)JsonSerializer.Deserialize(json, typeof(T), factory.Context);
+    }
+    public static async Task<T> LoadJsonFileAsync<T>(this IJsonFileSerializableFactory factory, Func<T> defaultValue,
         CancellationToken ct = default)
     {
+        ArgumentNullException.ThrowIfNull(factory);
+        ArgumentException.ThrowIfNullOrWhiteSpace(factory.FileName);
+        if (!File.Exists(factory.FileName))
+        {
+            return defaultValue();
+        }
+
         string json = await File.ReadAllTextAsync(factory.FileName, ct);
+        ArgumentException.ThrowIfNullOrWhiteSpace(json);
         return (T)JsonSerializer.Deserialize(json, typeof(T), factory.Context);
     }
 
     public static void SaveJsonFile<T>(this IJsonFileSerializableFactory factory, T obj)
     {
+        ArgumentNullException.ThrowIfNull(factory);
+        ArgumentException.ThrowIfNullOrWhiteSpace(factory.FileName);
+        ArgumentNullException.ThrowIfNull(obj);
         string json = JsonSerializer.Serialize(obj, typeof(T), factory.Context);
         File.WriteAllText(factory.FileName, json);
     }
@@ -62,12 +109,17 @@ public static class JsonSerializableExtensions
     public static async Task SaveJsonFileAsync<T>(this IJsonFileSerializableFactory factory, T obj,
         CancellationToken ct = default)
     {
+        ArgumentNullException.ThrowIfNull(factory);
+        ArgumentException.ThrowIfNullOrWhiteSpace(factory.FileName);
+        ArgumentNullException.ThrowIfNull(obj);
         string json = JsonSerializer.Serialize(obj, typeof(T), factory.Context);
         await File.WriteAllTextAsync(factory.FileName, json, ct);
     }
 
     public static string ToJson<T>(this IJsonSerializableFactory factory, T obj)
     {
+        ArgumentNullException.ThrowIfNull(factory);
+        ArgumentNullException.ThrowIfNull(obj);
         return JsonSerializer.Serialize(obj, typeof(T), factory.Context);
     }
 }
