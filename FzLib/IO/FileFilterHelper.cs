fbfabf072e3c99cdc6c8e6cdc436bb5aacf7813d
fshake <f-shake@outlook.com>
2025-11-20 15:09:38 +0800

    FileFilter支持了目录的筛选

--- FzLib/IO/FileFilterHelper.cs
+++ FzLib/IO/FileFilterHelper.cs
@@ -3,7 +3,6 @@ using System.Text.RegularExpressions;
 
 namespace FzLib.IO;
 
-
 public partial class FileFilterHelper
 {
     private static readonly char[] PathSplitter = ['/', '\\'];
@@ -87,12 +86,15 @@ public partial class FileFilterHelper
         }
     }
 
-    public bool IsMatched(string path)
+    public bool IsMatched(string path, bool isFile = true)
     {
-        string name = Path.GetFileName(path);
-        if (!IsMatchedName(name))
+        if (isFile)
         {
-            return false;
+            string name = Path.GetFileName(path);
+            if (!IsMatchedName(name))
+            {
+                return false;
+            }
         }
 
         path = path.Replace('\\', '/');
@@ -101,13 +103,15 @@ public partial class FileFilterHelper
             return false;
         }
 
-        string[] folders = Path.GetDirectoryName(path)?.Split(PathSplitter, StringSplitOptions.RemoveEmptyEntries);
+        string[] folders = isFile
+            ? Path.GetDirectoryName(path)?.Split(PathSplitter, StringSplitOptions.RemoveEmptyEntries)
+            : path.Split(PathSplitter, StringSplitOptions.RemoveEmptyEntries);
         return IsMatchedFolder(folders);
     }
 
     public bool IsMatched(FileSystemInfo file)
     {
-        return IsMatched(file.FullName);
+        return IsMatched(file.FullName, !file.Attributes.HasFlag(FileAttributes.Directory));
     }
 
     [GeneratedRegex(".*")]
