95d91ae1e99e91794c769b182db7991616f9b93c
f-shake <autodotua@outlook.com>
2026-04-27 20:05:35 +0800

    优化StringListConverter

--- FzLib.Avalonia/Converters/Basics/StringListConverter.cs
+++ FzLib.Avalonia/Converters/Basics/StringListConverter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Linq;
+using Avalonia;
 using Avalonia.Data.Converters;
 using FzLib.Text;
 
@@ -10,22 +11,40 @@ namespace FzLib.Avalonia.Converters
 {
     public class StringListConverter : IValueConverter
     {
-        public string DefaultSeparator { get; set; } = ",";
+        public string DefaultSeparator { get; set; } = ", ";
         public string[] AcceptedSeparator { get; set; } = [",", "，"];
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            if (value is null or UnsetValueType)
+            {
+                return string.Empty;
+            }
+
+            var separator = GetSeparator(parameter);
+
             if (value is IEnumerable<string> list)
             {
-                return string.Join(DefaultSeparator + " ", list) + DefaultSeparator + " ";
+                return string.Join(separator, list);
             }
 
             if (value is IEnumerable<EditableString> list2)
             {
-                return string.Join(DefaultSeparator + " ", list2.Select(p => p.Value)) + DefaultSeparator + " ";
+                return string.Join(separator, list2.Select(p => p.Value));
             }
 
             throw new Exception("值不是字符串数组或列表");
+        }
+
+        private string GetSeparator(object parameter)
+        {
+            string separator = DefaultSeparator;
+            if (parameter is string sep)
+            {
+                separator = sep;
+            }
+
+            return separator;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
@@ -45,8 +64,12 @@ namespace FzLib.Avalonia.Converters
                 throw new Exception("值不是字符串");
             }
 
+            var separator = GetSeparator(parameter);
+            HashSet<string> acceptedSeparator = [separator, ..AcceptedSeparator];
+
             var list = str
-                .Split(AcceptedSeparator, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                .Split(acceptedSeparator.ToArray(),
+                    StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
                 .Select(p => p.Trim());
 
             if (targetType == typeof(string[]))
